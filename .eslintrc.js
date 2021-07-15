d1af0b5543082b69feee4447678a9e6276adc850
Razunter <Razunter@gmail.com>
2021-07-15 19:16:03 +0400

    stylint, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 8
+    },
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    plugins: [],
+    // add your custom rules here
+    rules: {
+        'no-console': 0
+    }
+}

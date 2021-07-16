9d6a76ac01dd70105ce111a45c26f9ce95b7f541
Razunter <Razunter@gmail.com>
2021-07-16 10:19:58 +0400

    commented out Streamable for now

--- src/js/index.js
+++ src/js/index.js
@@ -21,9 +21,10 @@ function process() {
                 event.target.outerHTML = '<iframe src="https://www.youtube.com/embed/' + url + '?autoplay=1" width="480" height="360" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
             } else if (event.target.classList.contains('twitchclipThumb') || event.target.classList.contains('twitchThumb')) {
                 event.target.outerHTML = '<iframe src="https://player.twitch.tv/?video=v' + url + '&parent=raz-poebuilds.netlify.app" allowfullscreen="true" width="480" height="360" frameborder="0"></iframe>';
-            } else if (event.target.classList.contains('streamableThumb')) {
-                event.target.outerHTML = '<iframe src="' + url + '" frameborder="0" width="480" height="360" allowfullscreen frameborder="0"></iframe>';
             }
+            // else if (event.target.classList.contains('streamableThumb')) {
+            //     event.target.outerHTML = '<iframe src="' + url + '" frameborder="0" width="480" height="360" allowfullscreen frameborder="0"></iframe>';
+            // }
         });
     }
 }

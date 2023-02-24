07aca1a507a4920ab63a4c2228125008f896b2c1
Razunter <Razunter@gmail.com>
2023-02-24 17:51:49 +0400

    workaround for :where in Astro

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -4,13 +4,20 @@ const postcssLightningcss = require('postcss-lightningcss')
 const production = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  plugins: [
-    require('postcss-sort-media-queries'),
-    postcssLightningcss({
-      browsers: browserslist(),
-      lightningcssOptions: {
-        minify: production,
-      },
-    }),
-  ],
+    plugins: [
+        require('postcss-sort-media-queries'),
+        {
+            // workaround for :where
+            postcssPlugin: "inaccurate-pseudo-where",
+            Rule(rule) {
+                rule.selector = rule.selector?.replace(/:where\((\.astro-\w+)\)/g, "$1");
+            },
+        },
+        postcssLightningcss({
+            browsers: browserslist(),
+            lightningcssOptions: {
+                minify: production,
+            },
+        }),
+    ],
 };
